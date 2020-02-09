69f1bcbaeabea2e35f0a78ae1828d739367843d5
Vijay-Giri <lit2016038@iiitl.ac.in>
2020-02-09 23:17:16 +0530

    first commit

---
+++ todo/forms.py
@@ -0,0 +1,5 @@
+from django import forms
+
+class TodoForm(forms.Form):
+    text = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter todo e.g. Delete junk file', 'aria-label': 'Todo' ,'aria-describedby':'add-btn'}))
+
